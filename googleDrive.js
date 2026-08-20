7726dcfc00e396ec89ce749c2e6909c5b3468c7d
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-08-20 09:32:58 -0300

    Update googleDrive.js

--- googleDrive.js
+++ googleDrive.js
@@ -10,6 +10,29 @@ import { google } from "googleapis";
 
 import { Readable } from "stream";
 
+const oauth2Client =
+  new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+  );
+
+export function obtenerURLAutorizacionGoogle() {
+
+  return oauth2Client.generateAuthUrl({
+
+    access_type: "offline",
+
+    prompt: "consent",
+
+    scope: [
+      "https://www.googleapis.com/auth/drive"
+    ]
+
+  });
+
+}
+
 // ============================================================
 // AUTENTICACIÓN
 // ============================================================
