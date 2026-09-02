99c299615591122426114a63527ae267bf37b023
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-09-02 10:33:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ const verificarToken = (req, res, next) => {
   next();
 };
 
-.use(cors());
+app.use(cors());
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(express.json());
 app.use((req, res, next) => {
