e67b2bc7c3642b5c8a564bafcac33a224d9e7851
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-09-21 11:27:38 -0300

    Montar rutas RIC48 junto a protocolos

--- routes/ric39Routes.js
+++ routes/ric39Routes.js
@@ -7,7 +7,20 @@ import {
   enviarRIC39Drive
 } from "../controllers/ric39Controller.js";
 
+import {
+  guardarRIC48,
+  obtenerDetalleRIC48,
+  generarPDFRIC48,
+  enviarRIC48Drive
+} from "../controllers/ric48Controller.js";
+
 const router = express.Router();
+
+// RIC48 se monta antes de /:id para evitar colisiones con el detalle RIC39.
+router.post("/ric48", guardarRIC48);
+router.get("/ric48/:id", obtenerDetalleRIC48);
+router.get("/ric48/:id/pdf", generarPDFRIC48);
+router.post("/ric48/:id/drive", enviarRIC48Drive);
 
 router.post("/", guardarRIC39);
 router.get("/:id", obtenerDetalleRIC39);
