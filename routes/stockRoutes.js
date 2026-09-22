77c5292090a839025b48773945b637b264bd0360
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-09-22 08:19:05 -0300

    Aplicar cantidades enteras en stock

--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -29,28 +29,35 @@ import {
   ajustarExistenciaStock
 } from "../controllers/stockAjustesController.js";
 
+import {
+  normalizarRespuestaStockEntera,
+  validarCamposStockEnteros
+} from "../middleware/stockEnterosMiddleware.js";
+
 const router = express.Router();
+
+router.use(normalizarRespuestaStockEntera);
 
 // Categorías
 router.get("/categorias", listarStockCategorias);
 
 // Catálogo
 router.get("/items", listarStockItems);
-router.post("/items", crearStockItem);
+router.post("/items", validarCamposStockEnteros(["stock_minimo"]), crearStockItem);
 router.delete("/items/:id", eliminarStockItem);
 
 // Existencias
 router.get("/existencias", listarExistencias);
-router.post("/entradas", registrarEntradaStock);
-router.post("/salidas", registrarSalidaStock);
-router.put("/existencias/ajustar", ajustarExistenciaStock);
+router.post("/entradas", validarCamposStockEnteros(["cantidad"]), registrarEntradaStock);
+router.post("/salidas", validarCamposStockEnteros(["cantidad"]), registrarSalidaStock);
+router.put("/existencias/ajustar", validarCamposStockEnteros(["nueva_cantidad"]), ajustarExistenciaStock);
 
 // Movimientos
 router.get("/movimientos", listarMovimientosStock);
 
 // Transferencias
 router.get("/transferencias", listarTransferenciasStock);
-router.post("/transferencias", solicitarTransferenciaStock);
+router.post("/transferencias", validarCamposStockEnteros(["cantidad"]), solicitarTransferenciaStock);
 router.put("/transferencias/:id/resolver", resolverTransferenciaStock);
 
 // Cola de impresiones
