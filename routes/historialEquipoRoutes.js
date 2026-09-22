3d2a1334e7d44c62d404804cbecf26578d38261c
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-09-22 10:27:25 -0300

    Agregar endpoint público de equipos

--- routes/historialEquipoRoutes.js
+++ routes/historialEquipoRoutes.js
@@ -3,9 +3,11 @@ import {
   obtenerHistorialEquipo,
   obtenerDetalleRIC37
 } from "../controllers/historialEquipoController.js";
+import { obtenerEquipoPublico } from "../controllers/equipoPublicoController.js";
 
 const router = express.Router();
 
+router.get("/publico/:numero_serie", obtenerEquipoPublico);
 router.get("/:numero_serie/historial", obtenerHistorialEquipo);
 router.get("/ric37/:id", obtenerDetalleRIC37);
 
